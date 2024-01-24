70820ca2e92f86133ffb0f64f8d295eca11c69af
ramusiripalli <ramsiripalli2772@gmail.com>
2024-01-24 16:52:02 +0530

    Added mongo compass Url configuration

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/BlogApp").then(()=>{
+mongoose.connect("mongodb+srv://ramusiripalli2425:11502772@cluster0.e6o8ttl.mongodb.net/blogApplicationData").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
     console.log(err);
